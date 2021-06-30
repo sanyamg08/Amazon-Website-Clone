38200c7d31533b72c52b34e2aea717f66961709e
Sanyam Garg <56071939+sanyamg08@users.noreply.github.com>
2021-06-30 10:59:22 +0530

    Add files via upload

---
+++ server/routes/owner.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Owner = require("../models/owner");
+const multer  = require('multer');
+const {storage} = require('../middlewares/upload-photo');
+const upload = multer({ storage });
+
+// POST api
+router.post("/owners", upload.single("photo"), async (req, res) => {
+  try {
+    console.log(req.body);
+    let owner = new Owner();
+    owner.name = req.body.name;
+    owner.about = req.body.about;
+    owner.photo = req.file.location;
+    await owner.save();
+
+    res.json({
+      success: true,
+      message: "Successfully created a new owner"
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// GET api
+router.get("/owners", async (req, res) => {
+  try {
+    let owners = await Owner.find();
+
+    res.json({
+      owners: owners
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+module.exports = router;
---
+++ server/routes/product.js
@@ -0,0 +1,131 @@
+const router = require("express").Router();
+const Product = require("../models/product");
+const multer  = require('multer');
+const {storage} = require('../middlewares/upload-photo');
+const upload = multer({ storage });
+// POST request - create a new product
+
+// title: String,
+//   description: String,
+//   photo: String,
+//   price: Number,
+//   stockQuantity: Number,
+//   rating: [Number]
+
+router.post("/products", upload.single("photo"), async (req, res) => {
+  try {
+    let product = new Product();
+    console.log(req.body);
+   
+    product.price = req.body.price;
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.photo = req.file.location;
+    product.stockQuantity = req.body.stockQuantity;
+    product.owner = req.body.ownerID;
+    product.category = req.body.categoryID;
+  
+    await product.save(); // async
+    console.log(product);
+   
+    res.json({
+      status: true,
+      message: "Successfully saved"
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// GET requesst - get all products
+router.get("/products", async (req, res) => {
+  try {
+    let products = await Product.find()
+      .populate("owner","category")
+      .populate("reviews", "rating")
+      .exec();
+    res.json({
+      success: true,
+      products: products
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// GET request - get a single product
+router.get("/products/:id", async (req, res) => {
+  try {
+    let product = await Product.findOne({ _id: req.params.id })
+      .populate("owner category")
+      .populate("reviews", "rating")
+      .exec();
+    res.json({
+      success: true,
+      product: product
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// PUT request - Update a single product
+router.put("/products/:id", upload.single("photo"), async (req, res) => {
+  try {
+    let product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          price: req.body.price,
+          category: req.body.categoryID,
+          photo: req.file.location,
+          stockQuantity: req.body.stockQuantity,
+          description: req.body.description,
+          owner: req.body.ownerID
+        }
+      },
+      { upsert: true }
+    );
+
+    res.json({
+      success: true,
+      updatedProduct: product
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// DELETE request - delete a single product
+router.delete("/products/:id", async (req, res) => {
+  try {
+    let deletedProduct = await Product.findOneAndDelete({ _id: req.params.id });
+
+    if (deletedProduct) {
+      res.json({
+        status: true,
+        message: "Successfully deleted"
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+module.exports = router;
